9da1710e914c1f514bf73e24cb13742817dc09e1
KotaroYoshimatsu <kotaro.yoshimatsu@gmail.com>
2014-08-26 17:58:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,14 @@ wget http://coderepos.org/share/export/39278/lang/php/misc/dict.php
 mkdir -p ~/.vim/dictionaries/
 php dict.php | sort > ~/.vim/dictionaries/php.dict
 touch ~/.vimrc
+cat dotfiles.vimrc >> ~/.vimrc
+
+# for bashrc
+cd ~/
+wget https://storage.googleapis.com/golang/go1.3.1.linux-amd64.tar.gz
+tar xzf go1.3.1.linux-amd64.tar.gz
+cat dotfiles.bashrc >> ~/.bashrc
+source ~/.bashrc
+go get github.com/peco/peco/cmd/peco/
 
 
