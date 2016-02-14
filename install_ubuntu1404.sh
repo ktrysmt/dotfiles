12df370b36e66218e258d56334aa55643471e74d
keidrip <kotaro.yoshimatsu@gmail.com>
2016-02-14 14:04:33 +0000

    update install for ubuntu

--- install_ubuntu1404.sh
+++ install_ubuntu1404.sh
@@ -3,7 +3,7 @@ echo "You should install zsh and do that 'chsh -s /bin/zsh'";
 echo " "
 echo "sudo apt-get -y install zsh"
 echo "chsh -s /bin/zsh"
-echo 'exec $SHELL'
+echo 'su - <current_user>'
 echo "-----------------------------------------------------\n";
 sleep 3
 
@@ -11,18 +11,28 @@ echo "-----------------------------------------------------";
 echo "Update & install libraries";
 echo "-----------------------------------------------------\n";
 sudo apt-get -y update
-sudo apt-get -y install ctags curl zsh tig make gcc wget dstat silversearcher-ag
+sudo apt-get -y install ctags curl zsh tig make gcc dstat silversearcher-ag
 sudo apt-get -y install libssl-dev libcurl4-openssl-dev
 sudo apt-get -y install liblua5.2-dev lua5.2 python-dev ncurses-dev
 sudo apt-get -y install mercurial gettext libncurses5-dev libxmu-dev libgtk2.0-dev libperl-dev python-dev python3-dev ruby-dev tcl-dev
 sudo apt-get -y install luajit gccgo-go
 
 echo "-----------------------------------------------------";
-echo "Put files to cache dir";
+echo "Put files and dirs";
 echo "-----------------------------------------------------\n";
+# dotfiles
 mkdir ~/dotfiles
 wget -O ~/dotfiles/.zshrc https://raw.githubusercontent.com/keidrip/dotfiles/master/.zshrc
 wget -O ~/dotfiles/.vimrc https://raw.githubusercontent.com/keidrip/dotfiles/master/.vimrc.ubuntu
+# vim
+mkdir -p ~/.vim/bundle
+touch ~/.vimrc
+mkdir -p ~/.vim/plugin/
+# ghq
+echo "[ghq]
+  root = ~/project/src" > ~/.gitconfig
+source ~/.zshrc
+
 
 echo "-----------------------------------------------------";
 echo "Install latest git & tig";
@@ -32,7 +42,7 @@ wget https://www.kernel.org/pub/software/scm/git/git-2.6.3.tar.gz && \
 tar -zxf git-2.6.3.tar.gz && \
 cd git-2.6.3 && \
 make prefix=/usr/local all && \
-make prefix=/usr/local install
+sudo make prefix=/usr/local install
 cd ~/
 git clone https://github.com/jonas/tig
 cd tig
@@ -54,59 +64,57 @@ cd vim;
 make && sudo make install
 
 echo "-----------------------------------------------------";
-echo "Setup vimrc";
+echo "Install vim plugins";
 echo "-----------------------------------------------------\n";
 cd ~/
-mkdir -p ~/.vim/bundle
-touch ~/.vimrc
-cat ~/dotfiles/.vimrc >> ~/.vimrc
+cat ~/dotfiles/.vimrc > ~/.vimrc
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 wget --no-check-certificate https://raw.github.com/taku-o/downloads/master/visualmark.vim
-mkdir -p ~/.vim/plugin/
 mv visualmark.vim ~/.vim/plugin/
 
 echo "-----------------------------------------------------";
-echo "Setup oh-my-zsh";
+echo "Install zgen";
 echo "-----------------------------------------------------\n";
 cd ~/
 touch ~/.zshrc
-#curl -L http://install.ohmyz.sh | sh
 git clone https://github.com/tarjoilija/zgen ~/.zgen
-#git clone https://github.com/b4b4r07/zplug ~/.zplug
-cat ~/dotfiles/.zshrc >> ~/.zshrc
+cat ~/dotfiles/.zshrc > ~/.zshrc
 
 echo "-----------------------------------------------------";
-echo "Setup nodebrew for node (use stable:0.10)";
+echo "Install nodebrew for node (use stable:0.10)";
 echo "-----------------------------------------------------\n";
 cd ~/
 curl -L git.io/nodebrew | perl - setup
 nodebrew install-binary v0.10
 nodebrew use v0.10
-source ~/.zshrc
-npm install -g typescript typescript-tools
 
 echo "-----------------------------------------------------";
 echo "Install golang";
 echo "-----------------------------------------------------\n";
 wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz --no-check-certificate
 sudo tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
+
+echo "-----------------------------------------------------";
+echo "Apply .zshrc";
+echo "-----------------------------------------------------\n";
 source ~/.zshrc
 
 echo "-----------------------------------------------------";
-echo "Install golang utils";
+echo "Run go get";
 echo "-----------------------------------------------------\n";
 go get github.com/kr/godep
 go get github.com/peco/peco/cmd/peco
 go get github.com/motemen/ghq
-echo "[ghq]
-  root = ~/project/src" >> ~/.gitconfig
-source ~/.zshrc
 
 echo "-----------------------------------------------------";
-echo "Do NeoBundleInstall & GoInstallBinaries";
+echo "Run NeoBundleInstall & GoInstallBinaries";
 echo "-----------------------------------------------------\n";
-#vim +":NeoBundleInstall | :NeoBundleUpdate | :GoInstallBinaries" +:q
 vim +":NeoBundleInstall | :GoInstallBinaries" +:q
+
+echo "-----------------------------------------------------";
+echo "Run npm";
+echo "-----------------------------------------------------\n";
+npm install -g typescript typescript-tools
 
 # remove cache
 rm -rf ~/dotfiles
