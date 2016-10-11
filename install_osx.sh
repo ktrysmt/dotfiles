f69ad1293925f7c63bc1d35c7f77940b56f33af9
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2016-10-11 11:45:01 +0900

    Update install_osx.sh

--- install_osx.sh
+++ install_osx.sh
@@ -3,3 +3,4 @@ brew tap peco/peco
 brew install ctags lua wget tmux peco git zsh nkf tree the_silver_searcher
 brew install vim --with-lua
 git clone https://github.com/tarjoilija/zgen.git ~/.zgen
+# brew install chrome coteditor atom hyperswitch karabiner diffmerge firefox flux virtualbox vagrant 
