a84d8272ce0f5e03983ff4c09c739d7370c7ef8b
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2016-10-11 13:22:31 +0900

    Update install_osx.sh

--- install_osx.sh
+++ install_osx.sh
@@ -5,4 +5,4 @@ brew install vim --with-lua
 git clone https://github.com/tarjoilija/zgen.git ~/.zgen
 brew install Caskroom/cask/iterm2
 brew install Caskroom/cask/shiftit
-# brew install chrome coteditor atom hyperswitch karabiner diffmerge firefox flux virtualbox vagrant 
+# and install coteditor atom hyperswitch karabiner diffmerge flux virtualbox vagrant firefox
