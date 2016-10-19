2704268aec7a4050879c161918c2463f73a59ad5
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2016-10-19 21:10:39 +0900

    Update install_osx.sh

--- install_osx.sh
+++ install_osx.sh
@@ -35,7 +35,7 @@ export GOPATH=$HOME/project
 echo "#### Setup Other"
 go get github.com/motemen/ghq
 mkdir -p ~/.config/peco/
-cat ~/dotfiles/peco/config.json > ~/.config/peco/config.json
+cat ~dotfiles/.config/peco/config.json > ~/.config/peco/config.jso
 vim +":PlugInstall | :GoInstallBinaries" +:q
 
 echo "#### Rested tasks"
