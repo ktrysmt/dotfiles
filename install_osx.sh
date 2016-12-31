2e9844f41da407de2faeeeeb11a58adbf6acd8cf
ktrysmt <kotaro.yoshimatsu@gmail.com>
2016-12-31 19:10:19 +0900

    update install for ubuntu

--- install_osx.sh
+++ install_osx.sh
@@ -1,4 +1,6 @@
-echo "#### Install homebrew, softwares and libraries"
+echo "-----------------------------------------------------";
+echo "Install homebrew and libraries"
+echo "-----------------------------------------------------";
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew tap peco/peco
@@ -7,8 +9,12 @@ brew install reattach-to-user-namespace
 brew install vim --with-lua
 brew install Caskroom/cask/iterm2
 brew install Caskroom/cask/shiftit
+brew tap jhawthorn/fzy
+brew install fzy
 
-echo "#### Setup my env"
+echo "-----------------------------------------------------";
+echo "Setup my env"
+echo "-----------------------------------------------------";
 cd ~/
 git clone https://github.com/tarjoilija/zgen.git ~/.zgen
 git clone https://github.com/ktrysmt/dotfiles  ~/dotfiles
@@ -16,15 +22,21 @@ ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf.osx ~/.tmux.conf
+echo "[ghq]
+  root = ~/project/src" > ~/.gitconfig
 
-echo "#### Install Node"
+echo "-----------------------------------------------------";
+echo "Install Node"
+echo "-----------------------------------------------------";
 curl -L git.io/nodebrew | perl - setup
 ~/.nodebrew/nodebrew install-binary stable
 ~/.nodebrew/nodebrew use stable
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
-echo "#### Install Go"
-GOLANG_VERSION=1.7.1
+echo "-----------------------------------------------------";
+echo "Install Go"
+echo "-----------------------------------------------------";
+GOLANG_VERSION=1.7.2
 wget https://storage.googleapis.com/golang/go$GOLANG_VERSION.darwin-amd64.tar.gz --no-check-certificate
 tar -C /usr/local -xzf go$GOLANG_VERSION.darwin-amd64.tar.gz
 mkdir -p ~/project/bin
@@ -32,15 +44,18 @@ export PATH=$PATH:/usr/local/go/bin
 export PATH=$HOME/project/bin:$PATH
 export GOPATH=$HOME/project
 
-echo "#### Setup Other";
-echo "[ghq]
-  root = ~/project/src" > ~/.gitconfig
+echo "-----------------------------------------------------";
+echo "Setup Other";
+echo "-----------------------------------------------------";
 go get github.com/motemen/ghq
 mkdir -p ~/.config/peco/
 cat ~/dotfiles/.config/peco/config.json > ~/.config/peco/config.json
 vim +":PlugInstall | :GoInstallBinaries" +:q
 
-echo "#### Rested tasks"
+echo "-----------------------------------------------------";
+echo "Rested tasks"
+echo "-----------------------------------------------------";
 echo "1. chsh -s /bin/zsh"
 echo "2. source ~/.zshrc"
 echo "3. After, should install coteditor atom hyperswitch karabiner diffmerge flux virtualbox vagrant firefox, and Ricty diminished Font."
+
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,61 +1,28 @@
-GOLANG_VERSION=1.7.2
-
-echo "-----------------------------------------------------";
-echo "You should install zsh and do that 'chsh -s /bin/zsh'";
-echo " "
-echo "sudo apt-get -y install zsh"
-echo "chsh -s /bin/zsh"
-echo 'su - <current_user>'
-echo "-----------------------------------------------------\n";
-sleep 3
-
 echo "-----------------------------------------------------";
 echo "Update & install libraries";
 echo "-----------------------------------------------------\n";
 sudo apt-get -y update
-#sudo apt-get -y upgrade
-sudo apt-get -y install ctags curl zsh tig make gcc dstat silversearcher-ag
+sudo apt-get -y install git ctags curl zsh tig make gcc dstat silversearcher-ag
 sudo apt-get -y install libssl-dev libcurl4-openssl-dev
 sudo apt-get -y install liblua5.2-dev lua5.2 python-dev ncurses-dev
 sudo apt-get -y install mercurial gettext libncurses5-dev libxmu-dev libgtk2.0-dev libperl-dev python-dev python3-dev ruby-dev tcl-dev
-sudo apt-get -y install luajit gccgo-go
+sudo apt-get -y install luajit
 
 echo "-----------------------------------------------------";
-echo "Put files and dirs";
+echo "Setup my env"
 echo "-----------------------------------------------------\n";
-# dotfiles
-mkdir ~/dotfiles
-wget -O ~/dotfiles/.zshrc https://raw.githubusercontent.com/keidrip/dotfiles/master/.zshrc
-wget -O ~/dotfiles/.vimrc https://raw.githubusercontent.com/keidrip/dotfiles/master/.vimrc.ubuntu
-# vim
-mkdir -p ~/.vim/bundle
-touch ~/.vimrc
-mkdir -p ~/.vim/plugin/
-# ghq
+git clone https://github.com/tarjoilija/zgen.git ~/.zgen
+git clone https://github.com/ktrysmt/dotfiles  ~/dotfiles
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 echo "[ghq]
   root = ~/project/src" > ~/.gitconfig
-source ~/.zshrc
-
 
 echo "-----------------------------------------------------";
-echo "Install latest git & tig";
+echo "Install Vim with lua";
 echo "-----------------------------------------------------\n";
-cd ~/
-wget https://www.kernel.org/pub/software/scm/git/git-2.6.3.tar.gz && \
-tar -zxf git-2.6.3.tar.gz && \
-cd git-2.6.3 && \
-make prefix=/usr/local all && \
-sudo make prefix=/usr/local install
-cd ~/
-git clone https://github.com/jonas/tig
-cd tig
-make
-sudo make install
-
-echo "-----------------------------------------------------";
-echo "Install vim74";
-echo "-----------------------------------------------------\n";
-cd ~/
+cd /tmp/dotfiles
 git clone https://github.com/vim/vim
 cd vim;
 ./configure \
@@ -67,24 +34,9 @@ cd vim;
 make && sudo make install
 
 echo "-----------------------------------------------------";
-echo "Install vim plugins";
+echo "Install Go";
 echo "-----------------------------------------------------\n";
-cd ~/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cat ~/dotfiles/.vimrc > ~/.vimrc
-
-echo "-----------------------------------------------------";
-echo "Install zgen";
-echo "-----------------------------------------------------\n";
-cd ~/
-touch ~/.zshrc
-git clone https://github.com/tarjoilija/zgen.git ~/.zgen
-cat ~/dotfiles/.zshrc > ~/.zshrc
-cat ~/dotfiles/.zshenv > ~/.zshenv
-
-echo "-----------------------------------------------------";
-echo "Install golang";
-echo "-----------------------------------------------------\n";
+GOLANG_VERSION=1.7.2
 wget https://storage.googleapis.com/golang/go$GOLANG_VERSION.linux-amd64.tar.gz --no-check-certificate
 tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz
 mkdir -p ~/project/bin
@@ -93,33 +45,19 @@ export PATH=$HOME/project/bin:$PATH
 export GOPATH=$HOME/project
 
 echo "-----------------------------------------------------";
-echo "Install node;
-echo "-----------------------------------------------------\n";
+echo "Install Node"
+echo "-----------------------------------------------------";
 curl -L git.io/nodebrew | perl - setup
 ~/.nodebrew/nodebrew install-binary stable
 ~/.nodebrew/nodebrew use stable
+curl -o- -L https://yarnpkg.com/install.sh | bash
 
 echo "-----------------------------------------------------";
-echo "Run go get";
+echo "Setup Other";
 echo "-----------------------------------------------------\n";
 go get github.com/peco/peco/cmd/peco
 go get github.com/motemen/ghq
-
-echo "-----------------------------------------------------";
-echo "Setup peco";
-echo "-----------------------------------------------------\n";
 mkdir -p ~/.config/peco/
 cat ~/dotfiles/peco/config.json > ~/.config/peco/config.json
-
-echo "-----------------------------------------------------";
-echo "Run NeoBundleInstall & GoInstallBinaries";
-echo "-----------------------------------------------------\n";
 vim +":PlugInstall | :GoInstallBinaries" +:q
 
-echo "-----------------------------------------------------";
-echo "Apply .zshrc";
-echo "-----------------------------------------------------\n";
-source ~/.zshrc
-
-# remove cache
-rm -rf ~/dotfiles
