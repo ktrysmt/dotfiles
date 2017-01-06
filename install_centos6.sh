4593fe71398c318184db101188d89f29195cf885
ktrysmt <kotaro.yoshimatsu@gmail.com>
2017-01-06 17:01:37 +0900

    Update centos6

--- install_centos6.sh
+++ install_centos6.sh
@@ -1,47 +1,53 @@
-echo "----------------------------------------------------\n";
-echo "You should install zsh and do that 'chsh -s /bin/zsh'";
-echo "  "
-echo "sudo yum -y install zsh"
-echo "chsh -s /bin/zsh"
-echo 'exec $SHELL'
-echo "----------------------------------------------------\n";
-sleep 3
-
-# install general tools and libraries
+echo "-----------------------------------------------------";
+echo "Update & install libraries";
+echo "-----------------------------------------------------\n";
 sudo yum -y update
+sudo yum -y remove vim git
 sudo yum -y install epel-release
 sudo yum -y install ctags curl tig ncurses-devel make gcc wget php-cli openssl-devel perl-ExtUtils-MakeMaker gettext
 sudo yum -y install python-devel lua-devel dstat curl-devel zlib-devel
-sudo yum -y remove vim git
 
-# install zsh 4.3.17
+echo "-----------------------------------------------------";
+echo "Install Zsh, Git and tig"
+echo "-----------------------------------------------------\n";
+DOTFILES_TEMP_DIR=/tmp/dotfiles
+mkdir $DOTFILES_TEMP_DIR
+cd $DOTFILES_TEMP_DIR
 wget http://downloads.sourceforge.net/project/zsh/zsh-dev/4.3.17/zsh-4.3.17.tar.gz
 tar xvzf zsh-4.3.17.tar.gz
 cd zsh-4.3.17 && ./configure --enable-multibyte && make && sudo make install
 
-# install git,tig
-cd ~/
+cd $DOTFILES_TEMP_DIR
 wget https://www.kernel.org/pub/software/scm/git/git-2.6.3.tar.gz && \
 tar -zxf git-2.6.3.tar.gz && \
 cd git-2.6.3 && \
 ./configure && \
 make && \
 sudo make install
-source ~/.zshrc
 
-#cd ~/
-#git clone https://github.com/jonas/tig
-#cd tig
-#make
-#sudo make install
+cd $DOTFILES_TEMP_DIR
+git clone https://github.com/jonas/tig
+cd tig
+make
+sudo make install
 
-# put files to cache
-mkdir ~/dotfiles
-wget -O ~/dotfiles/.zshrc https://raw.githubusercontent.com/aqafiam/dotfiles/master/.zshrc
-wget -O ~/dotfiles/.vimrc https://raw.githubusercontent.com/aqafiam/dotfiles/master/.vimrc
-
-# install vim74
+echo "-----------------------------------------------------";
+echo "Setup my env"
+echo "-----------------------------------------------------\n";
 cd ~/
+git clone https://github.com/tarjoilija/zgen.git ~/.zgen
+git clone https://github.com/ktrysmt/dotfiles  ~/dotfiles
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+echo "[ghq]
+  root = ~/project/src" > ~/.gitconfig
+
+echo "-----------------------------------------------------";
+echo "Install Vim with lua";
+echo "-----------------------------------------------------\n";
+cd $DOTFILES_TEMP_DIR
 git clone https://github.com/vim/vim
 cd vim;
 ./configure \
@@ -52,71 +58,32 @@ cd vim;
  --disable-selinux \;
 make && sudo make install
 
-# setup vimrc
-cd ~/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cat ~/dotfiles/.vimrc >> ~/.vimrc
+echo "-----------------------------------------------------";
+echo "Install Go";
+echo "-----------------------------------------------------\n";
+GOLANG_VERSION=1.7.2
+wget https://storage.googleapis.com/golang/go$GOLANG_VERSION.linux-amd64.tar.gz --no-check-certificate
+tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz
+mkdir -p ~/project/bin
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$HOME/project/bin:$PATH
+export GOPATH=$HOME/project
 
-# setup dictionary for PHP
-wget "http://coderepos.org/share/browser/lang/php/misc/dict.php?format=txt" -O /tmp/dict.php
-mkdir -p ~/.vim/dictionaries/
-php /tmp/dict.php | sort > ~/.vim/dictionaries/php.dict
-
-# setup oh-my-zsh
-cd ~/
-touch ~/.zshrc
-git clone https://github.com/tarjoilija/zgen.git ~/.zgen
-cat ~/dotfiles/.zshrc >> ~/.zshrc
-source ~/.zshrc
-
-# setup nodebrew for node (use stable:0.10)
-cd ~/
+echo "-----------------------------------------------------";
+echo "Install Node"
+echo "-----------------------------------------------------";
 curl -L git.io/nodebrew | perl - setup
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshrc
-source ~/.zshrc
-nodebrew install-binary v4
-nodebrew use v4
-source ~/.zshrc
-npm install -g typescript typescript-tools
+~/.nodebrew/nodebrew install-binary stable
+~/.nodebrew/nodebrew use stable
+curl -o- -L https://yarnpkg.com/install.sh | bash
 
-# install golang
-wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz --no-check-certificate
-sudo tar -C /usr/local -xzf go1.5.linux-amd64.tar.gz
-source ~/.zshrc
-
-# install utils
-go get github.com/kr/godep
+echo "-----------------------------------------------------";
+echo "Setup Other";
+echo "-----------------------------------------------------\n";
 go get github.com/peco/peco/cmd/peco
 go get github.com/motemen/ghq
-touch ~/.gitconfig
-echo "[ghq]
-  root = ~/project/src" >> ~/.gitconfig
-
-# setup peco
 mkdir -p ~/.config/peco/
 cat ~/dotfiles/peco/config.json > ~/.config/peco/config.json
-
+vim +":PlugInstall | :GoInstallBinaries" +:q
 # ag
 sudo rpm -ivh http://swiftsignal.com/packages/centos/6/x86_64/the-silver-searcher-0.13.1-1.el6.x86_64.rpm
-
-# install vim-plugins
-#wget --no-check-certificate https://raw.github.com/taku-o/downloads/master/visualmark.vim
-#mkdir -p ~/.vim/plugin/
-#mv visualmark.vim ~/.vim/plugin/
-
-# setup vim-plugins
-vim +":PlugInstall | :GoInstallBinaries" +:q
-
-# tmux 1.8
-#cd ~/dotfiles
-#wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
-#tar xvzf libevent-2.0.21-stable.tar.gz
-#cd libevent-2.0.21-stable
-#./configure && make && make install
-#wget -O tmux-1.8.tar.gz "http://sourceforge.net/projects/tmux/files/tmux/tmux-1.8/tmux-1.8.tar.gz/download?use_mirror=jaist"
-#tar xvzf tmux-1.8.tar.gz
-#cd tmux-1.8
-#./configure && make && make install
-
-# remove cache
-rm -rf ~/dotfiles
