2f0175f97bcf2fed82a0187f988616a6eb849769
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2017-02-11 23:56:44 +0900

    Update install_ubuntu_desktop.sh

--- install_ubuntu_desktop.sh
+++ install_ubuntu_desktop.sh
@@ -10,12 +10,4 @@ sudo fc-cache -fv
 
 # applications
 sudo apt-get install launchy
-
-## chrome(1)
-sudo apt-get install libappindicator1
-echo "1. access to https://www.google.co.jp/chrome/browser/desktop/index.html"
-echo "2. install by dpkg: 'sudo dpkg -i ~/google-chrome-stable_current_amd64.deb'"
-## chrome(2)
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get install google-chrome-stable
+sudo apt-get install chromium-browser
