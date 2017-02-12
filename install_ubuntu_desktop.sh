886ce521fde0ec5117a73334ded33e044d8b77c5
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2017-02-12 12:44:26 +0900

    Update install_ubuntu_desktop.sh

--- install_ubuntu_desktop.sh
+++ install_ubuntu_desktop.sh
@@ -11,3 +11,13 @@ sudo fc-cache -fv
 # applications
 sudo apt-get install launchy
 sudo apt-get install chromium-browser
+
+# xkb
+echo 'partial hidden modifier_keys
+xkb_symbols "keymap" {
+    key <HENK> { [ Return ] };              // 変換をEnterに変更
+    key <MUHE> { [ BackSpace ] };           // 無変換をバックスペースに変更
+};' > ~/.xkb-user;
+sudo ln -s ~/.xkb-user /usr/share/X11/xkb/symbols/user
+echo '   user:keymap       =   +user(keymap)' >> /usr/share/X11/xkb/rules/evdev
+setxkbmap -option user:keymap
