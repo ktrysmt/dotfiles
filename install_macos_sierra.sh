bd391db3e7e714114582f6d23c1818a74b9a2913
ktrysmt <kotaro.yoshimatsu@gmail.com>
2018-09-08 02:05:13 +0900

    add bat

--- install_macos_sierra.sh
+++ install_macos_sierra.sh
@@ -8,8 +8,8 @@ echo "-----------------------------------------------------";
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 # general
-brew install git
-brew install peco wget tmux zsh nkf tree ripgrep reattach-to-user-namespace fd fzf tig fzy exa python jq git-secrets
+# brew install git
+brew install peco wget tmux zsh nkf tree ripgrep reattach-to-user-namespace fd fzf tig fzy exa python jq git-secrets bat
 brew install yarn --without-node
 brew install python@2
 # k8s
