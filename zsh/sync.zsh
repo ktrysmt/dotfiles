1d2172e54f476a44b137b97d0a250b080caf3577
ktrysmt <kotaro.yoshimatsu@gmail.com>
2022-02-18 13:18:54 +0900

    fix

--- zsh/sync.zsh
+++ zsh/sync.zsh
@@ -2,3 +2,13 @@ setopt AUTO_CD
 setopt interactivecomments
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+function peco-select-snippet() {
+  emulate -L zsh
+  BUFFER=$(cat ~/.snippet | peco)
+  CURSOR=$#BUFFER
+  zle -Rc
+  zle reset-prompt
+}
+zle -N peco-select-snippet
+bindkey '^X^M' peco-select-snippet
