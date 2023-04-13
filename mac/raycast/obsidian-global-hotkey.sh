5a6c77f6b0748131237c9905bb02bf36ac6685dc
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-04-14 06:21:27 +0900

    fix

---
+++ mac/raycast/obsidian-global-hotkey.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title obsidian global hotkey
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+Open -a "Obsidian"
+
