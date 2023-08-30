b63f2f42084e08014fa54812bde26de5b7dbad11
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-08-30 10:44:53 +0900

    wip

--- nvim/lua/plugins/fern.lua
+++ nvim/lua/plugins/fern.lua
@@ -11,28 +11,36 @@ return {
     vim.g["fern#disable_drawer_hover_popup"] = 1
     vim.g["fern_disable_startup_warnings"] = 1
 
-    vim.keymap.set('n', 'o', '<Plug>(fern-action-open-or-expand)', { remap = true })
-    vim.keymap.set('n', 'go', '<Plug>(fern-action-open:edit)<cmd><C-w>p', { remap = true })
-    vim.keymap.set('n', 't', '<Plug>(fern-action-open:tabedit)', { remap = true })
-    vim.keymap.set('n', 'T', '<Plug>(fern-action-open:tabedit)<cmd>gT', { remap = true })
-    vim.keymap.set('n', 'i', '<Plug>(fern-action-open:split)', { remap = true })
-    vim.keymap.set('n', 'gi', '<Plug>(fern-action-open:split)<cmd><C-w>p', { remap = true })
-    vim.keymap.set('n', 's', '<Plug>(fern-action-open:vsplit)', { remap = true })
-    vim.keymap.set('n', 'gs', '<Plug>(fern-action-open:vsplit)<cmd><C-w>p', { remap = true })
+    local function init_fern()
+      local o = { buffer = 0, remap = true }
+      vim.keymap.set('n', 's', 'gs', o)
+      vim.keymap.set('n', 'o', '<Plug>(fern-action-open-or-expand)', o)
+      vim.keymap.set('n', 't', '<Plug>(fern-action-open:tabedit)', o)
+      vim.keymap.set('n', 'T', '<Plug>(fern-action-open:tabedit)<cmd>gT', o)
+      vim.keymap.set('n', 'i', '<Plug>(fern-action-open:split)', o)
+      vim.keymap.set('n', 'gi', '<Plug>(fern-action-open:split)<cmd><C-w>p', o)
 
-    vim.cmd([[
-      nmap <buffer> <Plug>(fern-my-leave-and-tcd) <Plug>(fern-action-leave)<Plug>(fern-wait)<Plug>(fern-action-tcd:root)
-      nmap <buffer> u <Plug>(fern-my-leave-and-tcd)
-      nmap <buffer><expr>
-        \ <Plug>(fern-my-expand-or-collapse)
-        \ fern#smart#leaf(
-        \   "\<Plug>(fern-action-collapse)",
-        \   "\<Plug>(fern-action-expand:stay)",
-        \   "\<Plug>(fern-action-collapse)",
-        \ )
-      nmap <buffer>
-        \ <Plug>(fern-action-expand)
-        \ <Plug>(fern-my-expand-or-collapse)
-    ]])
+      vim.cmd([[
+        nmap <buffer> <Plug>(fern-my-leave-and-tcd) <Plug>(fern-action-leave)<Plug>(fern-wait)<Plug>(fern-action-tcd:root)
+        nmap <buffer> u <Plug>(fern-my-leave-and-tcd)
+        nmap <buffer><expr>
+          \ <Plug>(fern-my-expand-or-collapse)
+          \ fern#smart#leaf(
+          \   "\<Plug>(fern-action-collapse)",
+          \   "\<Plug>(fern-action-expand:stay)",
+          \   "\<Plug>(fern-action-collapse)",
+          \ )
+        nmap <buffer>
+          \ <Plug>(fern-action-expand)
+          \ <Plug>(fern-my-expand-or-collapse)
+      ]])
+    end
+
+    local fern_group = vim.api.nvim_create_augroup('fern_group', { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "fern",
+      group = fern_group,
+      callback = init_fern,
+    })
   end,
 }
