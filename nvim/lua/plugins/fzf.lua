a86a4ec134f1134d251f3e39ca02e6d335cb2af5
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-05 12:20:54 +0900

    fix fzf-lua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,16 +1,18 @@
 return {
   "ibhagwan/fzf-lua",
-  cmd = "FzfLua",
+  event = { "VimEnter", "BufReadPre", "BufNewFile" },
   config = function()
     local fzf = require("fzf-lua")
     fzf.setup({
       winopts = {
         height     = 0.85,     -- window height
-        width      = 0.80,     -- window width
-        row        = 0.35,     -- window row position (0=top, 1=bottom)
+        width      = 0.95,     -- window width
+        row        = 0.50,     -- window row position (0=top, 1=bottom)
         col        = 0.50,     -- window col position (0=left, 1=right)
-        border     = 'rounded', -- 'none', 'single', 'double', 'thicc' or 'rounded'
         fullscreen = false,    -- start fullscreen?
+        preview = {
+          horizontal = 'right:40%',
+        }
       },
     })
 
@@ -23,7 +25,7 @@ return {
     vim.opt.termguicolors = true
 
     local opt = { silent = true }
-    vim.keymap.set('n', '<leader>r', "<cmd>:FzfLua grep<CR>", opt)
+    vim.keymap.set('n', '<leader>r', "<cmd>:FzfLua grep<CR><CR>", opt)
     vim.keymap.set('n', '<leader>f', "<cmd>:FzfLua files<CR>", opt)
     vim.keymap.set('n', '<leader>b', "<cmd>:FzfLua buffers<CR>", opt)
     vim.keymap.set('n', '<leader>d', "<cmd>:FzfLua git_status<CR>", opt)
@@ -31,12 +33,6 @@ return {
     vim.keymap.set('n', '<leader>sh', "<cmd>:FzfLua search_history <CR>", opt)
     vim.keymap.set('n', '<leader>m', "<cmd>:FzfLua keymaps<CR>", opt)
     vim.keymap.set('n', '<leader>x', "<cmd>:FzfLua commands<CR>", opt)
-
-    vim.keymap.set('n', '<leader>d', "<cmd>lua require('fzf-lua').git_status()<CR>")
-    vim.keymap.set('n', '<leader>b', "<cmd>lua require('fzf-lua').git_branches()<CR>")
-    vim.keymap.set('n', '<leader>p', "<cmd>lua require('fzf-lua').grep()<CR>")
-    vim.keymap.set('n', '<leader>/', "<cmd>lua require('fzf-lua').blines()<CR>")
-
-    vim.keymap.set('n', '<leader>ld', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>")
+    vim.keymap.set('n', '<leader>ld', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", opt)
   end
 }
