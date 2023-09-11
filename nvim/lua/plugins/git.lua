2ed992fd0d7a6d4da3daffa0585a8d9a629e4b34
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-11 14:22:57 +0900

    fix

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,9 +2,18 @@ return {
   {
     "akinsho/git-conflict.nvim",
     event = { "VeryLazy" },
+    version = "*",
+    config = true
   },
   {
     'tpope/vim-fugitive',
     event = { "VeryLazy" },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { "VeryLazy" },
+    config = function()
+      require('gitsigns').setup()
+    end
   }
 }
