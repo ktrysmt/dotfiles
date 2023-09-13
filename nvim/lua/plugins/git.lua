daaa0cb3b93c56b65bb2560ae67a4ed37f4c2e03
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-13 16:15:37 +0900

    fix

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -13,11 +13,22 @@ return {
     'lewis6991/gitsigns.nvim',
     event = { "VeryLazy" },
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup({
+        signs = {
+          add          = { text = '+' },
+          change       = { text = '|' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        -- sign_priority = 100
+      })
 
       -- highlight GitSignsAddNr guibg=#173315 guifg=#626262
+      -- set signcolumn=yes:2
       vim.cmd [[
-      set signcolumn=yes:2
+      highlight GitSignsAdd guifg=#528832 guibg=#071905
       ]]
     end
   },
