6f55f4fefa369826edc3e930e15c3102bc440ce1
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-19 15:52:26 +0900

    remove

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,25 +1,64 @@
 return {
-  'williamboman/mason.nvim',
-  cmd = {
-    "Mason",
-    "MasonInstall",
-    "MasonUninstall",
-    "MasonUninstallAll",
-    "MasonLog",
-    "MasonUpdate",
-  },
-  opts = {
-    ui = {
-      check_outdated_packages_on_open = true,
-      icons = {
-        package_installed = "✓",
-        package_uninstalled = "✗",
-        package_pending = "⟳",
+  {
+    'williamboman/mason.nvim',
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+      "MasonUpdate",
+    },
+    opts = {
+      ui = {
+        check_outdated_packages_on_open = true,
+        icons = {
+          package_installed = "✓",
+          package_uninstalled = "✗",
+          package_pending = "⟳",
+        },
       },
     },
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end
   },
-  build = ":MasonUpdate",
-  config = function()
-    require("mason").setup()
-  end
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    cmd = {
+      "MasonToolsInstall",
+      "MasonToolsUpdate",
+    },
+    config = function()
+      local mason_tool_installer = require('mason-tool-installer')
+      mason_tool_installer.setup({
+        ensure_installed = {
+          'golangci-lint',
+          'bash-language-server',
+          'pylsp',
+          'lua-language-server',
+          'vim-language-server',
+          'gopls',
+          'marksman',
+          'typescript-language-server',
+          'stylua',
+          'shellcheck',
+          'dockerfile-language-server',
+          'gomodifytags',
+          'gotests',
+          'impl',
+          'lua-language-server',
+          'json-to-struct',
+          'misspell',
+          'revive',
+          'shellcheck',
+          'shfmt',
+          'staticcheck',
+          'vint',
+          'clangd',
+        },
+      })
+    end
+  }
 }
