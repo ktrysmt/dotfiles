3ff9e86dae7a759e5747d340bd9183c1332c55ac
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-20 15:03:31 +0900

    diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,10 +1,25 @@
 return {
   "sindrets/diffview.nvim",
   cmd = {
-    "DiffviewOpen",
     "DiffviewClose",
-    "DiffviewToggleFiles",
+    "DiffviewFileHistory",
     "DiffviewFocusFiles",
+    "DiffviewLog",
+    "DiffviewOpen",
     "DiffviewRefresh",
-  }
+    "DiffviewToggleFiles",
+  },
+  keys = {
+    { "<Leader>gv", "<cmd>DiffviewOpen<cr>",        mode = "n" },
+    { "<Leader>gh", "<cmd>DiffviewFileHistory<cr>", mode = "n" },
+  },
+  config = function()
+    require("diffview").setup({
+      file_history_panel = {
+        win_config = { -- See ':h diffview-config-win_config'
+          position = "left",
+        },
+      },
+    })
+  end
 }
