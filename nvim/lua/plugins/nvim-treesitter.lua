315b4a30b246b799284946414100248a56fb1280
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-24 12:11:53 +0900

    fix

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   event = "VeryLazy",
   build = ':TSUpdate',
+  dependencies = {
+    "yioneko/nvim-yati",
+  },
   config = function()
     -- require("nvim-treesitter.configs").setup({
     -- auto_install = true,
@@ -94,6 +97,21 @@ return {
         --     ['<leader>A'] = '@parameter.inner',
         --   },
         -- },
+      },
+      yati = {
+        enable = true,
+        -- Disable by languages, see `Supported languages`
+        -- disable = { "python" },
+
+        -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
+        default_lazy = true,
+
+        -- Determine the fallback method used when we cannot calculate indent by tree-sitter
+        --   "auto": fallback to vim auto indent
+        --   "asis": use current indent as-is
+        --   "cindent": see `:h cindent()`
+        -- Or a custom function return the final indent result.
+        default_fallback = "auto"
       },
     }
 
