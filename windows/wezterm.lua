01e4f616bbd6ecf1a122ca304ebb1f146c128fa6
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-27 11:04:43 +0900

    update wezterm

--- windows/wezterm.lua
+++ windows/wezterm.lua
@@ -3,17 +3,12 @@ local act = wezterm.action
 return {
   font = wezterm.font_with_fallback({
     { family = "Cica" },
-    --{family="Cica", weight="Bold"},
   }),
   font_size = 12,
   color_scheme = 'Ayu Mirage',
   hide_tab_bar_if_only_one_tab = true,
-  --line_height = 0.9,
-  --cell_width = 0.9,
   colors = {
-    -- The default text color
     foreground = 'white',
-    -- The default background color
     background = 'black',
   },
   window_padding = {
@@ -27,7 +22,6 @@ return {
   scrollback_lines = 35000,
   keys = {
     { key = 'v', mods = 'ALT',          action = act.PasteFrom 'Clipboard' },
-    { key = 'v', mods = 'ALT',          action = act.PasteFrom 'PrimarySelection' },
     { key = 'f', mods = 'CTRL | SHIFT', action = act.ToggleFullScreen },
   }
 }
