506bdea68d11d116549adb4ab9fda0f07b71f3af
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-09-29 21:47:16 +0900

    fix

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,8 +5,6 @@ return {
     require('lualine').setup {
       options = {
         theme = 'material',
-        -- component_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
         globalstatus = true,
       },
       sections = {
