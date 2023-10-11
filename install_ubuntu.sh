40430396e6fc660f65a839a722d396abd7c70416
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-10-11 20:31:36 +0900

    fix

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -131,7 +131,6 @@ if [[ "$(uname -r)" == *microsoft* ]]; then
   echo "alias pbpaste='powershell.exe Get-Clipboard'" >> ~/.zshrc.private
   echo 'export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ~/.zshrc.private
   echo 'export PATH=/home/linuxbrew/.linuxbrew/sbin:$PATH' >> ~/.zshrc.private
-  echo 'alias rm=gomi' >> ~/.zshrc.private
 else
   ln -s ~/dotfiles/.tmux.conf.ubuntu ~/.tmux.conf
 fi
