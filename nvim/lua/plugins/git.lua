f94294c4e5fa2c4cdf12a225ddeafa131558b36c
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-10-15 18:15:24 +0900

    update and fix gitconflict

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,9 +2,22 @@ return {
   {
     "akinsho/git-conflict.nvim",
     event = { "CursorHold", "CursorMoved", "ModeChanged", "InsertEnter", "CmdlineEnter", "CmdwinEnter" },
+    -- event = { "VeryLazy" },
     version = "*",
     config = function()
       require('git-conflict').setup()
+      local gitconflict_group = vim.api.nvim_create_augroup('gitconflict_group', { clear = true })
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'GitConflictDetected',
+        group = gitconflict_group,
+        callback = function()
+          vim.notify('Conflict detected in ' .. vim.fn.expand('<afile>'))
+          vim.keymap.set('n', 'cww', function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+          end)
+        end
+      })
     end
   },
   {
