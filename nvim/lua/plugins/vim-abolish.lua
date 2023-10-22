9af82f9abd2a901733b69b0b4cdd7ced712ec7c3
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-10-22 23:10:30 +0900

    fix

--- nvim/lua/plugins/vim-abolish.lua
+++ nvim/lua/plugins/vim-abolish.lua
@@ -1,4 +1,11 @@
 return {
   'tpope/vim-abolish',
-  event = { 'CmdlineEnter', 'CmdwinEnter' }
+  event = { 'CmdlineEnter', 'CmdwinEnter' }, -- :Subvert
+  keys = {
+    { "crs", mode = "n" },                   -- to snake_case
+    { "crc", mode = "n" },                   -- to camelCase
+    { "crk", mode = "n" },                   -- to kebab-case
+    { "cru", mode = "n" },                   -- to UPPER_CASE
+    { "crm", mode = "n" },                   -- to MixedCase
+  }
 }
