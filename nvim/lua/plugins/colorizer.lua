11621123c51518301739447fbe224d662878f6c8
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-11-10 11:01:42 +0900

    fix colorizer

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -2,7 +2,13 @@ return {
   'norcalli/nvim-colorizer.lua',
   event = { "CursorHold", "CursorMoved", "ModeChanged" },
   config = function()
-    require 'colorizer'.setup()
+    require 'colorizer'.setup({
+      '*',
+      css = {
+        rgb_fn = true,
+        names = true,
+      },
+    }, { names = false })
     vim.cmd [[ColorizerAttachToBuffer]]
   end
 }
