a6a19fc252a8fa5cc652188610003998264e80b5
ktrysmt <kotaro.yoshimatsu@gmail.com>
2023-12-06 20:52:08 +0900

    fix

--- install_mac.sh
+++ install_mac.sh
@@ -159,7 +159,8 @@ cp -Rp /System/Library/CoreServices/ScreenSaverEngine.app /Applications/lock.app
 # ------------
 # downloads
 # ---------------
-# PleckJP  https://github.com/ryota2357/PleckJP/releases
+# cica:    https://github.com/miiton/Cica/releases
+# PleckJP: https://github.com/ryota2357/PleckJP/releases
 # chrome:  https://www.google.com/intl/ja_jp/chrome/
 # ime:     https://www.google.co.jp/ime/
 # hackgen: brew tap homebrew/cask-fonts && brew install font-hackgen
