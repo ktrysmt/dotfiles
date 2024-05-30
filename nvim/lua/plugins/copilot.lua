a2d37d49e7f2130eb68838983ed2edd21420435e
ktrysmt <kotaro.yoshimatsu@gmail.com>
2024-05-30 16:23:29 +0900

    fix

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,5 +3,18 @@ return {
   event = { "CursorHold", "CursorMoved", "ModeChanged", "InsertEnter", "CmdlineEnter", "CmdwinEnter" },
   config = function()
     vim.keymap.set('i', '<Tab>', '<Plug>(copilot-accept-word)')
+
+    -- vim.g.copilot_filetypes = {
+    --   ["*"] = false,
+    --   ["javascript"] = true,
+    --   ["typescript"] = true,
+    --   ["lua"] = false,
+    --   ["rust"] = true,
+    --   ["c"] = true,
+    --   ["c#"] = true,
+    --   ["c++"] = true,
+    --   ["go"] = true,
+    --   ["python"] = true,
+    -- }
   end,
 }
