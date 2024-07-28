8c0fc8562ea14a2cf652ded7f093a24e514d48b9
ktrysmt <kotaro.yoshimatsu@gmail.com>
2024-07-28 03:18:34 +0000

    fix clipboard in vagrant of ubuntu

--- install/ubuntu.vagrant.sh
+++ install/ubuntu.vagrant.sh
@@ -8,9 +8,12 @@ sudo apt-get install Xvfb xsel -qq -y
 ln -s ~/dotfiles/.tmux.conf.ubuntu_vagrant ~/.tmux.conf
 
 cat <<EOF> ~/.zshrc.private
-if ! pgrep -f "Xvfb" > /dev/null; then
-  Xvfb -screen 0 1280x720x24 &
-  export DISPLAY=:0
+if [ -x /usr/bin/Xvfb ] && [ -x /usr/bin/VBoxClient ] && [ ! -f /tmp/.X0-lock ]; then
+  Xvfb -screen 0 320x240x8 > /dev/null 2>&1 &
+  sleep 0.5
+  export DISPLAY=":0"
   VBoxClient --clipboard
 fi
+alias pbcopy='xsel --display :0 --input --clipboard'
+alias pbpaste='xsel --display :0 --output --clipboard'
 EOF
