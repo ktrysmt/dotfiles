d0d82666e987a73a6a968d58802cd4df81058b2c
ktrysmt <kotaro.yoshimatsu@gmail.com>
2024-08-01 06:36:08 +0000

    fix

--- install/ubuntu.vagrant.sh
+++ install/ubuntu.vagrant.sh
@@ -12,7 +12,7 @@ if [ -x /usr/bin/Xvfb ] && [ -x /usr/bin/VBoxClient ] && [ ! -f /tmp/.X0-lock ];
   Xvfb -screen 0 320x240x8 > /dev/null 2>&1 &
   sleep 0.5
   export DISPLAY=":0"
-  VBoxClient --clipboard
+  sudo VBoxClient --clipboard
 fi
 alias pbcopy='xsel --display :0 --input --clipboard'
 alias pbpaste='xsel --display :0 --output --clipboard'
