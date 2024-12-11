64d0d863909f143714e288c416c87454cd81a6e4
ktrysmt <kotaro.yoshimatsu@gmail.com>
2024-12-11 15:38:55 +0000

    fix vbox

--- install/ubuntu/vagrant.sh
+++ install/ubuntu/vagrant.sh
@@ -8,10 +8,11 @@ sudo apt-get install xvfb xsel -qq -y
 ln -sf ~/dotfiles/.tmux.conf.ubuntu_vagrant ~/.tmux.conf
 
 cat << EOF > ~/.zshrc.private
+if ! pgrep -f VBoxClient > /dev/null; then
+  sudo VBoxClient --clipboard
+fi
 if [ -x /usr/bin/Xvfb ] && [ -x /usr/bin/VBoxClient ] && [ ! -f /tmp/.X0-lock ] && ! pgrep -f Xvfb > /dev/null; then
   Xvfb -screen 0 320x240x8 > /dev/null 2>&1 &
-  sleep 0.5
-  sudo VBoxClient --clipboard
 fi
 export DISPLAY=":0"
 alias pbcopy='xsel --display :0 --input --clipboard'
