337d09dd9250065229aa90d3394f18541f8f0625
ktrysmt <kotaro.yoshimatsu@gmail.com>
2025-06-09 20:47:01 +0900

    optimize guibg

--- nvim/lua/au.lua
+++ nvim/lua/au.lua
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd({ 'VimEnter', 'Colorscheme' }, {
   pattern = "*",
   group = highlight_group,
   command =
-  "hi IdeographicSpace ctermbg=DarkGreen guibg=DarkGreen | hi NormalNC guibg=#101010 | hi User1 guifg=#dddddd | hi @variable guifg=Normal"
+  "hi IdeographicSpace ctermbg=DarkGreen guibg=DarkGreen | hi NormalNC guibg=#171717 | hi User1 guifg=#dddddd | hi @variable guifg=Normal"
 })
 vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter' }, {
   pattern = "*",
