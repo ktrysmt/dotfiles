e4f589eefff54f91235e5a34f43e51ce412689f4
ktrysmt <kotaro.yoshimatsu@gmail.com>
2025-07-08 23:22:10 +0900

    fix

--- nvim/local.nvim/lua/local/command.lua
+++ nvim/local.nvim/lua/local/command.lua
@@ -1,38 +1,25 @@
 local M = {}
 
 function M.init()
-  -- vim.api.nvim_create_user_command("SayCommand", ':echo "command"', {})
+  -- vim.api.nvim_create_user_command("SayCommand", ':echo "command"', {}) -- hello_world
 
+  -- Rg
   vim.api.nvim_create_user_command('Rg', ':silent grep <args>', { nargs = '*', complete = file })
   if vim.fn.executable('rg') == 1 then
     vim.opt.grepprg = 'rg --vimgrep --no-heading --sort-files'
     vim.opt.grepformat = '%f:%l:%c:%m,%f:%l:%m'
   end
 
+  -- Rv,EV
   vim.api.nvim_create_user_command('Rv', ':source $MYVIMRC', {})
   vim.api.nvim_create_user_command('Ev', ':edit $HOME/dotfiles/nvim/init.lua', {})
 
+  -- w!! (sudo)
   vim.cmd([[
   cabbr w!! w !sudo tee > /dev/null %
   ]])
 
-  vim.cmd [[
-  " https://github.com/Happy-Dude/dotfiles/blob/main/vim/.vim/vimrc/functions.vim#L23-L48
-  " Usage :Pcre/pattern/replace/flags
-  if executable('perl') && has('nvim')
-    function s:PerlSubstitute(line1, line2, sstring)
-      let l:lines = getline(a:line1, a:line2)
-      let l:sysresult = systemlist("perl -CSDA -e 'use utf8;' -e '#line 1 \"PerlSubstitute\"' -pe ". shellescape("s".escape(a:sstring,"%!").";"), l:lines)
-      if v:shell_error
-        echo l:sysresult
-        return
-      endif
-      call nvim_buf_set_lines(0, a:line1 - 1, a:line2, v:false, l:sysresult)
-    endfunction
-    command! -range -nargs=1 Pcre call s:PerlSubstitute(<line1>, <line2>, <q-args>)
-  endif
-  ]]
-
+  -- cn
   vim.cmd [[
   function! s:search() abort
     let tmp = @"
--- nvim/lua/plugins/lexima.lua
+++ nvim/lua/plugins/lexima.lua
@@ -5,27 +5,21 @@ return {
     { "/", mode = "n" },
   },
   config = function()
-    vim.keymap.set("i", "<C-f>", "<Right>")
+    -- vim.keymap.set("i", "<C-f>", "<Right>")
 
     -- inoremap <C-f> <C-r>=lexima#insmode#leave(1, '<LT>C-G>U<LT>RIGHT>')<CR>
     vim.cmd [[
-
     function! s:lexima_alter_command_add_rule(original, alternative) abort
       let input_space = '<C-w>' . a:alternative . '<Right>'
       let input_cr    = '<C-w>' . a:alternative . '<CR>'
       let input_right = '<C-w>' . a:alternative . '<Right>'
-
       let rule = {
       \ 'mode': ':',
       \ 'at': '^\(''<,''>\)\?' . a:original . '\%#$',
       \ }
-
       call lexima#add_rule(extend(rule, { 'char': '<Space>', 'input': input_space }))
       call lexima#add_rule(extend(rule, { 'char': '<CR>',    'input': input_cr    }))
       call lexima#add_rule(extend(rule, { 'char': '<Right>', 'input': input_right }))
-
-
-      "  call lexima#add_rule({'char': '<C-f>', 'at': '\%#.*', 'leave': 1})
     endfunction
 
     if !exists('g:loaded_lexima_alter_command')
@@ -51,9 +45,6 @@ return {
     LeximaAlterCommand %s s/\v
     LeximaAlterCommand s s/\v
 
-    LeximaAlterCommand p\%[cre] Pcre/
-    LeximaAlterCommand %p\%[cre] Pcre/
-
     LeximaAlterCommand ti\%[gcurrent] tabnew|TigOpenCurrentFile
     LeximaAlterCommand ti\%[groot] tabnew|TigOpenProjectRootDir
 
@@ -65,7 +56,6 @@ return {
     LeximaAlterCommand oi Oil<space>.<cr>
 
     nnoremap / /\v
-
     ]]
   end
 }
