52220b76efe400ee7c067135dfe47fe1fd20c4b9
ktrysmt <kotaro.yoshimatsu@gmail.com>
2025-09-16 10:20:38 +0900

    opt wsl

--- install/ubuntu/wsl.sh
+++ install/ubuntu/wsl.sh
@@ -19,3 +19,6 @@ WIN_USER=${WIN_USER%$'\r'}
 ln -s /mnt/c/Users/$WIN_USER/home_scripts/win32yank.exe /usr/local/bin/win32yank.exe
 # https://github.com/kaz399/spzenhan.vim/blob/master/zenhan/spzenhan.exe
 ln -s /mnt/c/Users/$WIN_USER/home_scripts/spzenhan.exe /usr/local/bin/spzenhan.exe
+
+# https://github.com/git-ecosystem/git-credential-manager/releases/tag/v2.6.1
+git config --global credential.helper "/mnt/c/Program\ Files\ \(x86\)/Git\ Credential\ Manager/git-credential-manager.exe"
