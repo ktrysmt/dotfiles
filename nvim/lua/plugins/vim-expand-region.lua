5a9d89220521e538de606d4c69f5d3c77af57104
ktrysmt <kotaro.yoshimatsu@gmail.com>
2025-12-31 21:16:01 +0900

    add

--- nvim/lua/plugins/vim-expand-region.lua
+++ nvim/lua/plugins/vim-expand-region.lua
@@ -9,8 +9,23 @@ return {
   },
   config = function()
     vim.cmd [[
-      vmap v <Plug>(expand_region_expand)
-      vmap V <Plug>(expand_region_shrink)
+    let g:expand_region_text_objects = {
+    \ 'iw'  :0,
+    \ 'iW'  :0,
+    \ 'i"'  :0,
+    \ 'i''' :0,
+    \ 'ia'  :0,
+    \ 'i)'  :1,
+    \ 'il'  :1,
+    \ 'if'  :1,
+    \ 'af'  :1,
+    \ 'it'  :1,
+    \ 'ie'  :0,
+    \ }
+    ]]
+    vim.cmd [[
+    vmap v <Plug>(expand_region_expand)
+    vmap V <Plug>(expand_region_shrink)
     ]]
   end
 }
