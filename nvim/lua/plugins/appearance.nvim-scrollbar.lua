04387b61b666b8d5f2bf03deab77c4a29cb9219e
ktrysmt <kotaro.yoshimatsu@gmail.com>
2026-01-02 17:12:54 +0900

    fix hlslens

--- nvim/lua/plugins/appearance.nvim-scrollbar.lua
+++ nvim/lua/plugins/appearance.nvim-scrollbar.lua
@@ -1,35 +1,16 @@
 return {
   'petertriho/nvim-scrollbar',
-  commit = "5b103ef0fd2e8b9b4be3878ed38d224522192c6c",
   event = { "VeryLazy" },
   dependencies = {
     {
       'kevinhwang91/nvim-hlslens',
-      -- tag = "v1.1.0",
     },
   },
   config = function()
-    -- これを先に呼ぶことでhlslens virtual text を無効化
+    require("hlslens").setup()
+    require("scrollbar").setup({ handlers = { search = true } })
     require("scrollbar.handlers.search").setup({
       override_lens = function() end,
-    })
-
-    require("scrollbar").setup({
-      excluded_buftypes = {
-        'nofile', -- to disable in floating windows
-      },
-      excluded_filetypes = {
-        "cmp_docs",
-        "cmp_menu",
-        "prompt",
-      },
-      set_highlights = true,
-      handlers = {
-        cursor = true,
-        diagnostic = true,
-        handle = true,
-        search = true, -- Requires hlslens
-      },
     })
 
     vim.cmd [[
