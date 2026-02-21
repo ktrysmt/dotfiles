1d8eb045d9287fb73158edd19b7ae4883d0f837e
ktrysmt <kotaro.yoshimatsu@gmail.com>
2026-02-21 16:46:54 +0900

    up

--- .claude/statusline-command.sh
+++ .claude/statusline-command.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 input=$(cat)
-cwd=$(echo "$input" | jq -r '.cwd' | sed "s|^$HOME|~|")
-printf "\033[01;34m%s\033[00m" "$cwd"
+
+MODEL=$(echo "$input" | jq -r '.model.display_name')
+DIR=$(echo "$input" | jq -r '.cwd' | sed "s|^$HOME|~|")
+PCT=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
+
+echo "${DIR##*/} | $MODEL | ${PCT}% context"
