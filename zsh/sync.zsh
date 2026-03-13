008d9d604e80d3cf59a3d57193834fc9a3416802
ktrysmt <kotaro.yoshimatsu@gmail.com>
2026-03-13 11:47:07 +0900

    up

--- zsh/sync.zsh
+++ zsh/sync.zsh
@@ -30,7 +30,8 @@ export DIRSTACKSIZE=100
 # bat
 export BAT_THEME=gruvbox-dark
 # fzf
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git | sort'
+# To manage exclusions globally, use ~/.fdignore instead of --exclude flags
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude .venv | sort'
 export FZF_DEFAULT_OPTS="--reverse --height ${FZF_TMUX_HEIGHT:-80%} --select-1 --exit-0 --tiebreak=index"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'"
