ce50915737ade98f5fd30314411113bd22adbd6a
ktrysmt <kotaro.yoshimatsu@gmail.com>
2026-03-29 18:03:36 +0900

    fix memd

--- nvim/lua/plugins/memd.lua
+++ nvim/lua/plugins/memd.lua
@@ -1,12 +1,12 @@
 return {
-  -- "ktrysmt/memd.nvim",
-  dir = "~/workspace/memd.nvim", -- for development
+  "ktrysmt/memd.nvim",
+  -- dir = "~/workspace/memd.nvim", -- for development
   event = "VeryLazy",
   keys = {
-    { '<leader>md', mode = "n", desc = 'Memd: Toggle preview' },
+    { '<leader>mm', mode = "n", desc = 'Memd: Toggle preview' },
   },
   config = function()
     require('memd').setup()
-    vim.keymap.set('n', '<leader>md', require('memd').toggle, { desc = 'Memd: Toggle preview' })
+    vim.keymap.set('n', '<leader>mm', require('memd').toggle, { desc = 'Memd: Toggle preview' })
   end,
 }
